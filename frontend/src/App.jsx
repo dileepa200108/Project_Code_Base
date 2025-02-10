a4569b027de1883780ab55a209cb0a89ea301bc6
Dileepa <113455403+dileepa200108@users.noreply.github.com>
2025-02-10 17:46:41 +0530

    frontend fixed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from "@nextui-org/react";
+
+
+const App = () => {
+
+
+
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+console.log("Vite config loaded!");
+
+export default defineConfig({
+  plugins: [
+    react(),
+  ],
+});
